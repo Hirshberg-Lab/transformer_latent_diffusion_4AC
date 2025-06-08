8b151dbedbe42accc773b76631a19e48e7354de3
sagimeir <sagim00@gmail.com>
2025-06-08 18:28:31 +0300

    I do not need this file, maybe in the future

--- tld/data.py
+++ tld/data.py
@@ -8,14 +8,14 @@ import os
 from dataclasses import asdict
 from typing import List, Union
 
-import clip
+# import clip
 import h5py
 import numpy as np
 import pandas as pd
 import torch
 import torchvision.transforms as transforms
 import webdataset as wds
-from diffusers.models.autoencoders.autoencoder_kl import AutoencoderKL
+# from diffusers.models.autoencoders.autoencoder_kl import AutoencoderKL
 from img2dataset import download
 from torch import Tensor, nn
 from torch.utils.data import DataLoader
@@ -25,28 +25,28 @@ from tld.configs import DataDownloadConfig
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-@torch.no_grad()
-def encode_text(label: Union[str, List[str]], model: nn.Module, device: str | torch.device) -> Tensor:
-    text_tokens = clip.tokenize(label, truncate=True).to(device)
-    text_encoding = model.encode_text(text_tokens)
-    return text_encoding.cpu()
+# @torch.no_grad()
+# def encode_text(label: Union[str, List[str]], model: nn.Module, device: str | torch.device) -> Tensor:
+#     text_tokens = clip.tokenize(label, truncate=True).to(device)
+#     text_encoding = model.encode_text(text_tokens)
+#     return text_encoding.cpu()
 
 
-@torch.no_grad()
-def encode_image(img: Tensor, vae: AutoencoderKL) -> Tensor:
-    x = img.to(device).to(torch.float16)
+# @torch.no_grad()
+# def encode_image(img: Tensor, vae: AutoencoderKL) -> Tensor:
+#     x = img.to(device).to(torch.float16)
 
-    x = x * 2 - 1  # to make it between -1 and 1.
-    encoded = vae.encode(x, return_dict=False)[0].sample()
-    return encoded.cpu()
+#     x = x * 2 - 1  # to make it between -1 and 1.
+#     encoded = vae.encode(x, return_dict=False)[0].sample()
+#     return encoded.cpu()
 
 
-@torch.no_grad()
-def decode_latents(out_latents: torch.Tensor, vae: AutoencoderKL) -> Tensor:
-    # expected to be in the unscaled latent space
-    out = vae.decode(out_latents.to(device))[0].cpu()
+# @torch.no_grad()
+# def decode_latents(out_latents: torch.Tensor, vae: AutoencoderKL) -> Tensor:
+#     # expected to be in the unscaled latent space
+#     out = vae.decode(out_latents.to(device))[0].cpu()
 
-    return ((out + 1) / 2).clip(0, 1)
+#     return ((out + 1) / 2).clip(0, 1)
 
 
 def quantize_latents(lat: Tensor, clip_val: float = 20) -> Tensor:
@@ -67,111 +67,111 @@ def append_to_dataset(dataset: h5py.Dataset, new_data: Tensor) -> None:
     dataset[-new_data.shape[0] :] = new_data
 
 
-def get_text_and_latent_embeddings_hdf5(
-    dataloader: DataLoader, vae: AutoencoderKL, model: nn.Module, drive_save_path: str
-) -> None:
-    """Process img/text inptus that outputs an latent and text embeddings and text_prompts, saving encodings as float16."""
+# def get_text_and_latent_embeddings_hdf5(
+#     dataloader: DataLoader, vae: AutoencoderKL, model: nn.Module, drive_save_path: str
+# ) -> None:
+#     """Process img/text inptus that outputs an latent and text embeddings and text_prompts, saving encodings as float16."""
 
-    img_latent_path = os.path.join(drive_save_path, "image_latents.hdf5")
-    text_embed_path = os.path.join(drive_save_path, "text_encodings.hdf5")
-    metadata_csv_path = os.path.join(drive_save_path, "metadata.csv")
+#     img_latent_path = os.path.join(drive_save_path, "image_latents.hdf5")
+#     text_embed_path = os.path.join(drive_save_path, "text_encodings.hdf5")
+#     metadata_csv_path = os.path.join(drive_save_path, "metadata.csv")
 
-    with h5py.File(img_latent_path, "a") as img_file, h5py.File(text_embed_path, "a") as text_file:
-        if "image_latents" not in img_file:
-            img_ds = img_file.create_dataset(
-                "image_latents",
-                shape=(0, 4, 32, 32),
-                maxshape=(None, 4, 32, 32),
-                dtype="float16",
-                chunks=True,
-            )
-        else:
-            img_ds = img_file["image_latents"]
+#     with h5py.File(img_latent_path, "a") as img_file, h5py.File(text_embed_path, "a") as text_file:
+#         if "image_latents" not in img_file:
+#             img_ds = img_file.create_dataset(
+#                 "image_latents",
+#                 shape=(0, 4, 32, 32),
+#                 maxshape=(None, 4, 32, 32),
+#                 dtype="float16",
+#                 chunks=True,
+#             )
+#         else:
+#             img_ds = img_file["image_latents"]
 
-        if "text_encodings" not in text_file:
-            text_ds = text_file.create_dataset(
-                "text_encodings", shape=(0, 768), maxshape=(None, 768), dtype="float16", chunks=True
-            )
-        else:
-            text_ds = text_file["text_encodings"]
+#         if "text_encodings" not in text_file:
+#             text_ds = text_file.create_dataset(
+#                 "text_encodings", shape=(0, 768), maxshape=(None, 768), dtype="float16", chunks=True
+#             )
+#         else:
+#             text_ds = text_file["text_encodings"]
 
-        for img, (label, url) in tqdm(dataloader):
-            text_encoding = encode_text(label, model, device).cpu().numpy().astype(np.float16)
-            img_encoding = encode_image(img, vae).cpu().numpy().astype(np.float16)
+#         for img, (label, url) in tqdm(dataloader):
+#             text_encoding = encode_text(label, model, device).cpu().numpy().astype(np.float16)
+#             img_encoding = encode_image(img, vae).cpu().numpy().astype(np.float16)
 
-            append_to_dataset(img_ds, img_encoding)
-            append_to_dataset(text_ds, text_encoding)
+#             append_to_dataset(img_ds, img_encoding)
+#             append_to_dataset(text_ds, text_encoding)
 
-            metadata_df = pd.DataFrame({"text": label, "url": url})
-            if os.path.exists(metadata_csv_path):
-                metadata_df.to_csv(metadata_csv_path, mode="a", header=False, index=False)
-            else:
-                metadata_df.to_csv(metadata_csv_path, mode="w", header=True, index=False)
+#             metadata_df = pd.DataFrame({"text": label, "url": url})
+#             if os.path.exists(metadata_csv_path):
+#                 metadata_df.to_csv(metadata_csv_path, mode="a", header=False, index=False)
+#             else:
+#                 metadata_df.to_csv(metadata_csv_path, mode="w", header=True, index=False)
 
 
-def download_and_process_data(
-    latent_save_path="latents",
-    raw_imgs_save_path="raw_imgs",
-    csv_path="imgs.csv",
-    image_size=256,
-    bs=64,
-    caption_col="captions",
-    url_col="url",
-    download_data=True,
-    number_sample_per_shard=10000,
-):
-    if not os.path.exists(raw_imgs_save_path):
-        os.mkdir(raw_imgs_save_path)
+# def download_and_process_data(
+#     latent_save_path="latents",
+#     raw_imgs_save_path="raw_imgs",
+#     csv_path="imgs.csv",
+#     image_size=256,
+#     bs=64,
+#     caption_col="captions",
+#     url_col="url",
+#     download_data=True,
+#     number_sample_per_shard=10000,
+# ):
+#     if not os.path.exists(raw_imgs_save_path):
+#         os.mkdir(raw_imgs_save_path)
 
-    if not os.path.exists(latent_save_path):
-        os.mkdir(latent_save_path)
+#     if not os.path.exists(latent_save_path):
+#         os.mkdir(latent_save_path)
 
-    if download_data:
-        download(
-            processes_count=8,
-            thread_count=64,
-            url_list=csv_path,
-            image_size=image_size,
-            output_folder=raw_imgs_save_path,
-            output_format="webdataset",
-            input_format="csv",
-            url_col=url_col,
-            caption_col=caption_col,
-            enable_wandb=False,
-            number_sample_per_shard=number_sample_per_shard,
-            distributor="multiprocessing",
-            resize_mode="center_crop",
-        )
+#     if download_data:
+#         download(
+#             processes_count=8,
+#             thread_count=64,
+#             url_list=csv_path,
+#             image_size=image_size,
+#             output_folder=raw_imgs_save_path,
+#             output_format="webdataset",
+#             input_format="csv",
+#             url_col=url_col,
+#             caption_col=caption_col,
+#             enable_wandb=False,
+#             number_sample_per_shard=number_sample_per_shard,
+#             distributor="multiprocessing",
+#             resize_mode="center_crop",
+#         )
 
-    files = os.listdir(raw_imgs_save_path)
-    tar_files = [os.path.join(raw_imgs_save_path, file) for file in files if file.endswith(".tar")]
-    print(tar_files)
-    dataset = wds.WebDataset(tar_files)
+#     files = os.listdir(raw_imgs_save_path)
+#     tar_files = [os.path.join(raw_imgs_save_path, file) for file in files if file.endswith(".tar")]
+#     print(tar_files)
+#     dataset = wds.WebDataset(tar_files)
 
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-        ]
-    )
+#     transform = transforms.Compose(
+#         [
+#             transforms.ToTensor(),
+#         ]
+#     )
 
-    # output is (img_tensor, (caption , url_col)) per batch:
-    dataset = (
-        dataset.decode("pil")
-        .to_tuple("jpg;png", "json")
-        .map_tuple(transform, lambda x: (x["caption"], x[url_col]))
-    )
+#     # output is (img_tensor, (caption , url_col)) per batch:
+#     dataset = (
+#         dataset.decode("pil")
+#         .to_tuple("jpg;png", "json")
+#         .map_tuple(transform, lambda x: (x["caption"], x[url_col]))
+#     )
 
-    dataloader = DataLoader(dataset, batch_size=bs, shuffle=False)
+#     dataloader = DataLoader(dataset, batch_size=bs, shuffle=False)
 
-    model, _ = clip.load("ViT-L/14")
+#     model, _ = clip.load("ViT-L/14")
 
-    vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
-    vae = vae.to(device)
-    model.to(device)
+#     vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+#     vae = vae.to(device)
+#     model.to(device)
 
-    print("Starting to encode latents and text:")
-    get_text_and_latent_embeddings_hdf5(dataloader, vae, model, latent_save_path)
-    print("Finished encode latents and text:")
+#     print("Starting to encode latents and text:")
+#     get_text_and_latent_embeddings_hdf5(dataloader, vae, model, latent_save_path)
+#     print("Finished encode latents and text:")
 
 
 def main(data_config: DataDownloadConfig):
