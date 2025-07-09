f4b73f09a214652b32c4319c218053e37e4b174a
sagimeir <sagim00@gmail.com>
2025-07-09 16:29:24 +0300

    added normalization per channel to the stft

--- rnd_spectra/ontheflydataset.py
+++ rnd_spectra/ontheflydataset.py
@@ -17,7 +17,11 @@ class OnTheFlyDataset(Dataset):
                  dtype: str = 'float32',
                  normalize_inputs: Optional[tuple[float,float]] = (0.,1.),
                  normalize_labels: Optional[tuple[float,float]] = (0.,1.),
-                 use_stft: bool = False
+                 use_stft: bool = False,
+                 stft_C_mean: torch.Tensor = torch.Tensor([1.9e-02, -1.4e-06]),
+                 stft_C_std: torch.Tensor = torch.Tensor([1.8, 0.16]),
+                 stft_G_mean: torch.Tensor = torch.Tensor([2.59e-02, -1.07e-05]),
+                 stft_G_std: torch.Tensor = torch.Tensor([1.4, 0.338])
                  ) -> None:
         
         self.laplace = Laplace(bumps)
@@ -29,8 +33,8 @@ class OnTheFlyDataset(Dataset):
         self.mean_C ,self.std_C = normalize_inputs
 
         if use_stft:
-            self.encoding_G = STFT(hop_length=1)
-            self.encoding_C = STFT()
+            self.encoding_G = STFT(hop_length=1,mean=stft_G_mean,std=stft_G_std)
+            self.encoding_C = STFT(mean=stft_C_mean,std=stft_C_std)
         else:
             self.encoding_G = nn.Identity()
             self.encoding_C = nn.Identity()
@@ -55,7 +59,9 @@ class OnTheFlyDataset(Dataset):
 class STFT(nn.Module):
     def __init__(self, n_fft: int = 90,
                  hop_length: int = 9, 
-                 win_length: int = 14
+                 win_length: int = 14,
+                 mean: torch.Tensor = torch.Tensor([0, 0]),
+                 std: torch.Tensor = torch.Tensor([1, 1])
                  ) -> None:
         super().__init__()
         self.n_fft = n_fft
@@ -65,8 +71,10 @@ class STFT(nn.Module):
         self.rearrange = Rearrange(
             "b freq time z -> b z freq time",
         )
+        self.mean = mean[None, :, None, None]
+        self.std = std[None, :, None, None]
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # x: (batch, x_points)
+        
         x= torch.stft(
             x,
             n_fft=self.n_fft,
@@ -76,13 +84,17 @@ class STFT(nn.Module):
             return_complex=True
         )
         x = torch.view_as_real(x)
-        return self.rearrange(x).squeeze(0)
+        x = self.rearrange(x)
+        x = (x - self.mean) / self.std
+        return x.squeeze(0)
 
 class ISTFT(nn.Module):
     def __init__(self, n_fft: int = 90, 
                  hop_length: int = 9, 
                  win_length: int = 14,
-                 length: int = 1024
+                 length: int = 1024,
+                 mean: torch.Tensor = torch.Tensor([0, 0]),
+                 std: torch.Tensor = torch.Tensor([1, 1])
                  ) -> None:
         super().__init__()
         self.n_fft = n_fft
@@ -94,8 +106,10 @@ class ISTFT(nn.Module):
             "b z freq time -> b freq time z",
             z=2,
         )
+        self.mean = mean[None, :, None, None]
+        self.std = std[None, :, None, None]
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # x: (batch, x_points)
+        x = (x * self.std) + self.mean        
         x = self.rearrange(x).contiguous() # this way all dimensions of returned tensor will have stride 1. see https://stackoverflow.com/questions/63852258/how-can-i-get-a-view-of-input-as-a-complex-tensor-runtimeerror-tensor-must-hav
         x = torch.view_as_complex(x)
         return torch.istft(
